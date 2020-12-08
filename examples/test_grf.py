bf28c80df3a9a7a346dcef7171df1a02df601ae6
Guochao Sun <jsun.astro@gmail.com>
2020-12-08 14:16:49 -0800

    minor updates

--- examples/test_grf.py
+++ examples/test_grf.py
@@ -3,6 +3,5 @@ import numpy as np
 import starterlite
 
 # Read in fiducial true [CII] power spectrum
-pscii_data = np.load(os.getenv('STARTER')+
-                     '/examples/TimeScienceMaster/data/CIIPS_TRUE_z%s_MAR_%.1e.npz'%(str(z_ps).replace('.','p'), ps.M_thres))
+pscii_data = np.load(os.getenv('STARTERLITE')+'/input/CIIPS_TRUE_z7p1_MAR_1.0e+08.npz'))
 ps3d_cii_true = interp1d(pscii_data['k'], pscii_data['ps'], kind='linear', bounds_error=False, fill_value=(0., 0.))
